c30783340210c83036cc9b6f9296ae4844dfdeee
EkawaD <55327310+EkawaD@users.noreply.github.com>
2020-12-09 17:43:03 +0100

    slect + graph

--- graph.py
+++ graph.py
@@ -7,8 +7,23 @@ class Graph:
     def __init__(self):
         self.df = pd.read_csv('data/responses.csv')
 
+    @classmethod
+    def save_plot(cls, name):
+        plt.savefig('static/graph/' + name + '.jpg', pad_inches=1, bbox_inches='tight')
+
     @property
-    def music_on_loneliness(self):
+    def music_lonely(self):
+        self.create_plot_music_lonely()
+        self.save_plot('music_lonely')
+        return '/static/graph/music_lonely.jpg'
+
+    @property
+    def music_happy(self):
+        self.create_plot_music_happy()
+        self.save_plot('music_happy')
+        return '/static/graph/music_happy.jpg'
+
+    def create_plot_music_lonely(self):
         lonely5 = self.df[self.df['Loneliness'] == 5]
         lonely1 = self.df[self.df['Loneliness'] == 1]
         ln1_mean = lonely1.iloc[:, 2:19].mean().sort_values(ascending=False)
@@ -18,8 +33,15 @@ class Graph:
         tab = all_mean.rename(columns={0: 'Loneliness1', 1: 'Average', 2: 'Loneliness5'})
         h_var = tab[abs(tab['Loneliness1'] - tab['Loneliness5']) > 0.1]
         h_var.plot.bar()
-        plt.savefig('static/graph/tab_lonely.jpg', pad_inches=1, bbox_inches='tight')
 
-        return '/static/graph/tab_lonely.jpg'
-
-
+    def create_plot_music_happy(self):
+        df = self.df
+        not_happy = df[df['Happiness in life'] <= 2]
+        happy = df[df['Happiness in life'] >= 4]
+        not_hp_mean = not_happy.iloc[:, 2:19].mean().sort_values(ascending=False)
+        hp_mean = happy.iloc[:, 2:19].mean().sort_values(ascending=False)
+        all_mean = df.iloc[:, 2:19].mean().sort_values(ascending=False)
+        all_mean = pd.concat([not_hp_mean, all_mean, hp_mean], axis=1)
+        tab = all_mean.rename(columns={0: 'Not Happy', 1: 'Average', 2: 'Happy'})
+        h_var = tab[abs(tab['Not Happy'] - tab['Happy']) >= 0.3]
+        h_var.sort_values('Average', ascending=False).plot.bar()
--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-import io
-
 from flask import Flask, render_template, request
 from graph import Graph
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('formulaire.html')
+    return render_template('home.html', url=' ')
 
 
-@app.route('/home', methods=['POST'])
-def resultat():
-    result = request.form
-    d = result['music']
-    return render_template("home.html", music=d)
-
-
-@app.route('/hello/')
+@app.route('/select_graph/', methods=['POST'])
 def hello():
     g = Graph()
-    img = g.music_on_loneliness
-
-    return render_template("plot.html", url=img)
+    result = request.form['graph']
+    if result == 'Happy':
+        img = g.music_happy
+    else:
+        img = g.music_lonely
+    return render_template("home.html", url=img)
 
 
 ''' 
-1. Créer des sélecteurs > template 'jinja' et des formulaires 
-2. routes avec des requêtes POST pour récupérer les infos sélectionnés 
-3. Display les graphiques en fonction des sélecteurs.
-    1. Créer les graphiques
-    2. Sauvegarder les graphiques sur local
-    3. Display graphique as IMG 
-
-
 4. Compléter le README
 5. Compléter notebook 
 '''
