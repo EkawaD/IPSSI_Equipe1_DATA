0a83f8e64a544ff4546a5cada28f2f8b9283ba7d
EkawaD <55327310+EkawaD@users.noreply.github.com>
2020-12-09 17:04:23 +0100

    flask + pandas

---
+++ graph.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class Graph:
+
+    def __init__(self):
+        self.df = pd.read_csv('data/responses.csv')
+
+    @property
+    def music_on_loneliness(self):
+        lonely5 = self.df[self.df['Loneliness'] == 5]
+        lonely1 = self.df[self.df['Loneliness'] == 1]
+        ln1_mean = lonely1.iloc[:, 2:19].mean().sort_values(ascending=False)
+        average_mean = self.df.iloc[:, 2:19].mean().sort_values(ascending=False)
+        ln5_mean = lonely5.iloc[:, 2:19].mean().sort_values(ascending=False)
+        all_mean = pd.concat([ln1_mean, average_mean, ln5_mean], axis=1)
+        tab = all_mean.rename(columns={0: 'Loneliness1', 1: 'Average', 2: 'Loneliness5'})
+        h_var = tab[abs(tab['Loneliness1'] - tab['Loneliness5']) > 0.1]
+        h_var.plot.bar()
+        plt.savefig('static/graph/tab_lonely.jpg', pad_inches=1, bbox_inches='tight')
+
+        return '/static/graph/tab_lonely.jpg'
+
+    def 
--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-import flask
+import io
+
+from flask import Flask, render_template, request
+from graph import Graph
+
+app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    pass
+@app.route('/')
+def home():
+    return render_template('formulaire.html')
+
+
+@app.route('/home', methods=['POST'])
+def resultat():
+    result = request.form
+    d = result['music']
+    return render_template("home.html", music=d)
+
+
+@app.route('/hello/')
+def hello():
+    g = Graph()
+    img = g.music_on_loneliness
+
+    return render_template("plot.html", url=img)
+
+
+''' 
+1. Créer des sélecteurs > template 'jinja' et des formulaires 
+2. routes avec des requêtes POST pour récupérer les infos sélectionnés 
+3. Display les graphiques en fonction des sélecteurs.
+    1. Créer les graphiques
+    2. Sauvegarder les graphiques sur local
+    3. Display graphique as IMG 
+
+
+4. Compléter le README
+5. Compléter notebook 
+'''
